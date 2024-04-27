bec39a39c361fd87d463ceb5ee1c043ea30f484c
thenagain <thena9ain@gmail.com>
2024-04-27 21:49:34 +0900

    🎉 init project

---
+++ SampleApp/SampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Takahiro Nagai on 2024/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
